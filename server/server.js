80177b3a431801fe83e71668986e15ce8f06c06a
Hadrien Allegaert <hadrien.allegaert@student.uclouvain.be>
2020-10-28 10:27:30 +0100

    refact call back in anonymous fucntion

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ const upload = multer({});
 let MongoClient = require('mongodb').MongoClient;
 let http = require('http');
 let https = require('https');
-let bcrypt = require('bcrypt');
 let fs= require('fs');
 
 // DIY modules
@@ -35,38 +34,39 @@ app.use(session({
     }
 }));
 
-MongoClient.connect(dbUrl, {useUnifiedTopology: true}, (err, db) => {
+MongoClient.connect(dbUrl, {useUnifiedTopology: true}, (err, db)=>{
     if(err) {
         console.log("-----------  ERROR  -----------");
         throw err;
     } else {
         console.log("---------- CONNECTED ----------");
+
         // redirect localhost:8080 to localhost:8080/main.html
         app.get('/', function(req, res){
-            db.db('olln').collection('reports').find({}).toArray((err, doc) =>{
+            db.db('olln').collection('reports').find({}).toArray((err, doc)=>{
                 if(err) throw err;
                 if(req.session.pseudo === undefined) res.render('../server/views/index', {reports: doc});
                 else res.render('../server/views/index', {pseudo: req.session.pseudo, reports: doc});
             });
         });
 
-        app.get('/log', function(req, res, next){
+        app.get('/log', (req, res, next)=>{
             if(req.session.pseudo !== undefined)res.redirect('/');
             else res.render('../server/views/login', {cookie: req.session.showCookieAlert})
         });
 
         // pattern for login post method
-        app.post('/login', function (req, res) {
+        app.post('/login', (req, res)=>{
             login(req, res, db);
         });
 
         // pattern for sign up method
-        app.post('/sign', function(req, res){
+        app.post('/sign', (req, res)=>{
             sign(req, res, db);
         });
 
         // pre-build function to debug
-        app.get('/report', function(req, res){
+        app.get('/report', (req, res)=>{
             if(req.session.pseudo === undefined) res.redirect('/log');
             else res.render('../server/views/report');
         });
@@ -75,14 +75,14 @@ MongoClient.connect(dbUrl, {useUnifiedTopology: true}, (err, db) => {
            report(req, res, db);
         });
 
-        app.get('/disconnect', function(req, res){
+        app.get('/disconnect', (req, res)=>{
             delete req.session._id;
             delete req.session.mail;
             delete req.session.pseudo;
             res.redirect('/');
         });
 
-        app.post('/ajaxCookieAlert', (req, res) => {
+        app.post('/ajaxCookieAlert', (req, res)=>{
             if(req.body.showCookieAlert === "false"){
                 req.session.showCookieAlert = req.body.showCookieAlert;
                 res.status(200).send('Alert will not appear again.');
@@ -91,14 +91,14 @@ MongoClient.connect(dbUrl, {useUnifiedTopology: true}, (err, db) => {
     }
 });
 
-//listen server port
+//listen server port 443
 https.createServer({
     key: fs.readFileSync('./server/key.pem'),
     cert: fs.readFileSync('./server/server.crt'),
 }, app).listen(443);
 
-// Redirect from http port 80 to https
-http.createServer((req, res) => {
+// redirect from http port 80 to https port 443
+http.createServer((req, res)=>{
     res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
     res.end();
 }).listen(80);
