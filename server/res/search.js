8cb071b53d1178275f7d0171a8fb16f36de98cc1
Hadrien Allegaert <hadrien.allegaert@student.uclouvain.be>
2020-11-11 11:41:17 +0100

    regex with replacer last update

--- server/res/search.js
+++ server/res/search.js
@@ -1,21 +1,19 @@
-const regex = /[`@#$%^*()_+\-=[\]{};':"\\|,.<>\/?]/;
+const regex = /[&\/\\#,+()$~%.'":*?<>{}\[\]]/g;
 
 module.exports = function search(req, res, db){
-    if(regex.test(req.body.search)) res.redirect('/');
-    else {
-        db.db('olln').collection('reports').find({
-            $or:
-                [
-                    {description: {$regex: req.body.search, $options: '$i'}},
-                    {date: {$regex: req.body.search, $options: '$i'}},
-                    {localisation: {$regex: req.body.search, $options: '$i'}},
-                    {author: {$regex: req.body.search, $options: '$i'}}
-                ]
-        }).toArray((err, doc) => {
-            if (err) throw err;
-            else {
-                res.render('../server/views/index.ejs', {reports: doc});
-            }
-        });
-    }
+    if(regex.test(req.body.search)) req.body.search = req.body.search.replace(regex, ' ').trim();
+    db.db('olln').collection('reports').find({
+        $or:
+            [
+                {description: {$regex: req.body.search, $options: '$i'}},
+                {date: {$regex: req.body.search, $options: '$i'}},
+                {localisation: {$regex: req.body.search, $options: '$i'}},
+                {author: {$regex: req.body.search, $options: '$i'}}
+            ]
+    }).toArray((err, doc) => {
+        if (err) throw err;
+        else {
+            res.render('../server/views/index.ejs', {reports: doc});
+        }
+    });
 };
